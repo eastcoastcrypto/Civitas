10795afd646879f914b43f7b461748a5e53cef32
Vetro7 <ericvesprini@yahoo.com>
2018-01-11 18:13:16 -0500

    Update civitasstrings.cpp

--- src/qt/civitasstrings.cpp
+++ src/qt/civitasstrings.cpp
@@ -56,7 +56,7 @@ QT_TRANSLATE_NOOP("civitas-core", ""
 "Distributed under the MIT software license, see the accompanying file "
 "COPYING or <http://www.opensource.org/licenses/mit-license.php>."),
 QT_TRANSLATE_NOOP("civitas-core", ""
-"Enable automatic wallet backups triggered after each zPhr minting (0-1, "
+"Enable automatic wallet backups triggered after each zCiv minting (0-1, "
 "default: %u)"),
 QT_TRANSLATE_NOOP("civitas-core", ""
 "Enable spork administration functionality with the appropriate private key."),
