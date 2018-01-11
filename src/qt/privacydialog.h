4f7bf34368a5b2b435942af59ed4e63483525c09
Vetro7 <ericvesprini@yahoo.com>
2018-01-11 14:22:17 -0500

    Update privacydialog.h

--- src/qt/privacydialog.h
+++ src/qt/privacydialog.h
@@ -46,7 +46,7 @@ public:
 
     void setModel(WalletModel* model);
     void showOutOfSyncWarning(bool fShow);
-    void setZPhrControlLabels(int64_t nAmount, int nQuantity);
+    void setZCivControlLabels(int64_t nAmount, int nQuantity);
 
 public slots:
     void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance, 
@@ -100,7 +100,7 @@ private slots:
     void on_pushButtonMintReset_clicked();
     void on_pushButtonSpentReset_clicked();
     void on_pushButtonSpendzCIV_clicked();
-    void on_pushButtonZPhrControl_clicked();
+    void on_pushButtonZCivControl_clicked();
     void on_pasteButton_clicked();
     void updateDisplayUnit();
 };
