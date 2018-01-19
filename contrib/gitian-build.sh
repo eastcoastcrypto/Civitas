f8cbc51ec3ac72aa33a8f0bf71523b9f53d95a4f
Jamison Phillips <jamisoncphillips@gmail.com>
2018-01-19 07:58:58 +0000

    updated url in gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/civitasproject/civitas
+url=https://github.com/eastcoastcrypto/Civitas
 proc=2
 mem=2000
 lxc=true
@@ -39,7 +39,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/civitasproject/civitas
+-u|--url	Specify the URL of the repository. Default is https://github.com/eastcoastcrypto/Civitas
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -237,8 +237,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/civitasproject/gitian.sigs.git
-    git clone https://github.com/civitasproject/civitas-detached-sigs.git
+    git clone https://github.com/eastcoastcrypto/gitian.sigs.git
+    git clone https://github.com/eastcoastcrypto/civitas-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
@@ -252,6 +252,7 @@ then
 fi
 
 # Set up build
+echo `pwd`
 pushd ./civitas
 git fetch
 git checkout ${COMMIT}
