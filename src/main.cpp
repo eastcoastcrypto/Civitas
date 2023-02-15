eb23c7f5531dd0a3b67bf64077fb2bd3cedeecb9
The Botfather <SloRunner@users.noreply.github.com>
2023-02-15 18:26:04 +0100

    Add checkpoints and new spork to enforce the version

--- src/main.cpp
+++ src/main.cpp
@@ -6299,7 +6299,11 @@ int ActiveProtocol()
     // SPORK_15 is used for 70911. Nodes < 70911 don't see it and still get their protocol version via SPORK_14 and their
     // own ModifierUpgradeBlock()
 
-    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
+    // if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
+    //        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
+    // return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
+
+    if (IsSporkActive(SPORK_17_NEW_PROTOCOL_ENFORCEMENT_3))
             return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
